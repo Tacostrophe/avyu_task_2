c36aef1c5ea553ea217c2d120bd2c37f86f8c175
Дмитрий Кутин <TacoKong@yandex.ru>
2022-12-09 06:32:42 +0300

    card model created

--- avyu_task_2/api/models.py
+++ avyu_task_2/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
 
-# Create your models here.
+
+class Card(models.model):
+    '''Модель карты'''
+    class CardStatuses(models.TetxChoices):
+        '''Возможные статусы карты'''
+        ACTIVE = 'AC', gettext_lazy('Active')
+        INACTIVE = 'IA', gettext_lazy('Inactive')
+        EXPIRED = 'EX', gettext_lazy('Expired')
+
+    series = models.IntegerField()
+    number = models.IntegerField()
+    date_release = models.DateTimeField()
+    date_expiration = models.DateTimeField()
+    status = models.CharField(
+        max_length=2,
+        choices=CardStatuses.choices,
+        default=CardStatuses.INACTIVE
+    )
